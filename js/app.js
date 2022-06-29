dbeaa32f85aaddd038d178017ce27b10f7ab52fd
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-06-29 19:10:06 -0300

    Se agregó efecto parallax al header

--- js/app.js
+++ js/app.js
@@ -4,9 +4,13 @@ const stars = document.getElementById("stars");
 const moon = document.getElementById("moon");
 const mountains_behind = document.getElementById("mountains_behind");
 const mountains_front = document.getElementById("mountains_front");
+
 const btn_explore = document.getElementById("btn-explore");
 const text = document.getElementById("text");
 const sec = document.getElementById("sec");
+const header = document.getElementById("header");
+
+
 
 window.addEventListener("scroll", ()=>{
     let value_scroll = window.scrollY;
@@ -16,5 +20,11 @@ window.addEventListener("scroll", ()=>{
     moon.style.top = value_scroll/1.5 +"px";
     mountains_behind.style.top = value_scroll * 0.2 +"px";
     mountains_front.style.top = value_scroll * -0.1 +"px";
+
+    text.style.marginRight = value_scroll/2+"px" 
+    text.style.marginTop = value_scroll /2 + "px" 
+    btn_explore.style.marginTop = value_scroll /2 + "px" 
+
+    header.style.top = value_scroll / 1.3  + "px";
 });
 
